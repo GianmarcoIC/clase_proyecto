9602374dbd3ae76011d6caa6a66799f405baacf4
GianmarcoIC <i2415363@continental.edu.pe>
2024-11-26 14:55:57 -0500

    Update sem7.py

--- sem7.py
+++ sem7.py
@@ -8,7 +8,7 @@ from sklearn.metrics import mean_squared_error
 
 # Configurar Supabase
 SUPABASE_URL = "https://msjtvyvvcsnmoblkpjbz.supabase.co"
-SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9..."
+SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1zanR2eXZ2Y3NubW9ibGtwamJ6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzIwNTk2MDQsImV4cCI6MjA0NzYzNTYwNH0.QY1WtnONQ9mcXELSeG_60Z3HON9DxSZt31_o-JFej2k"
 
 # Crear cliente Supabase
 try:
